a72f9dec2b19b9b4f5c421ec739d560a03a86528
Benedek Kiss <kissbene@gmail.com>
2025-03-02 14:05:11 +0100

    Upload test data

--- src/main.py
+++ src/main.py
@@ -7,6 +7,39 @@ from ai.generate import generate
 from utils.file_loader import saveResult
 
 
+topics = [
+    "Accessory digestive organs",
+    "Alimentary canal organs",
+    "Anti Inflammatory Drugs",
+    "Antimicrobials",
+    "Connective Tissue",
+    "Epithelium",
+    "Esophagus",
+    "Histology of the peripheral nervous system",
+    "Hypersensitivies and autoimmune diseases",
+    "Kidney",
+    "Lipid Lowering Drugs",
+    "Medical Histology",
+    "Oral Cavity and Salivary Glands",
+    "Phagocytic Cells",
+    "Skin and Glands",
+    "Transplantation",
+    "Vasoactive Drugs for hypertension",
+    "VDJ recombination",
+    "Acute coronary syndromes",
+    "Hypertension",
+    "Fibroids",
+    "Placenta",
+    "Mitral stenosis",
+    "Aortic regurgitation",
+    "Trophoblastic neoplasia",
+    "Cervical neoplasia",
+    "Aortic stenosis",
+    "Chronic coronary artery disease",
+    "Female pelvic anatomy",
+    "Ovarian pathology",
+]
+
 inputPath = os.environ.get("INPUT_PATH")
 outputPath = os.environ.get("OUTPUT_PATH")
 
@@ -15,6 +48,8 @@ def handleTopic(client: genai.Client, path: str, depth:int = 0):
 
     fullPath = os.path.join(inputPath, path)
     mainTopicName = os.path.basename(os.path.normpath(fullPath))
+    if mainTopicName.split(" - ")[1] not in topics:
+        return
     outPath, _ = os.path.split(os.path.normpath(os.path.join(outputPath, path)))
     resultPath = os.path.join(outPath, f"{mainTopicName}.json")
     addRequest(client, fullPath, resultPath, mainTopicName, depth)
